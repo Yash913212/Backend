98fc0b4c197386fba42adbe88d2fff99fdd92e47
Yaswanth__7 <yaswanthamjuri@gmail.com>
2025-12-05 11:10:17 +0530

    Fix server configuration and callback issues

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import http from 'http'
 const server = http.createServer((req, res) => {
     console.log(req.method);
-    if(req.url === "/users"){
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify({name: "Yash"}))
-    }else{
-        res.writeHead(200, {"Content-Type": "text/plane"});
+    if (req.url === "/users") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ name: "Yash" }))
+    } else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Hello this is from backend")
     }
 });
-server.listen(2000, ()=>{
-    console.log(`This server is running at ${2000}`)
+server.listen(3000, () => {
+    console.log(`This server is running at ${3000}`)
 });
